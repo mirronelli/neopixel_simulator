03fc73409bb5590ef18446e591aadd45243722ad
Miroslav Moravek <miroslav.moravek@2ring.com>
2021-12-19 17:29:08 +0100

    gamma to effects

--- effects/common.py
+++ effects/common.py
@@ -14,3 +14,6 @@ def color_function(i, steps, max):
         return int(max * (1 - (i - 3*sixth) / sixth)) 
     # /¯¯\__
     return 0
+
+def create_gamma_table(gamma):
+    return [round(pow(i / 255, gamma) * 255) for i in range(256)]

22f667be70a3b4c9bbfaf656322798d806deb427
Miroslav Moravek <miroslav.moravek@2ring.com>
2021-12-19 13:54:53 +0100

    added rpi launcher

---
+++ rpi_run.py
@@ -0,0 +1,21 @@
+import time
+from rpi_ws281x import Color, PixelStrip, ws
+from effects import snake
+
+LED_COUNT = 95         # Number of LED pixels.
+LED_PIN = 12           # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ = 800000   # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10           # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255   # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False     # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0
+LED_STRIP = ws.SK6812_STRIP_RGBW
+SLEEP = 100
+
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+strip.begin()
+
+effect = snake.Snake(strip, 30, 8, 255, 0, 0, 0)
+while True:
+    effect.next_frame()
+    strip.show()

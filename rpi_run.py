a01ff378c10ecafe9b517fef82ce1ade221b26f1
Mirronelli <mirronelli@hotmail.com>
2022-03-06 15:09:22 +0000

    updated light count

--- rpi_run.py
+++ rpi_run.py
@@ -3,7 +3,7 @@ from rpi_ws281x import PixelStrip, ws
 from effects import snake, rainbowBursts, rainbowLine, redGreen, common
 from effects.effect_factory import create_all_list
 
-LED_COUNT = 95         # Number of LED pixels.
+LED_COUNT = 304         # Number of LED pixels.
 LED_PIN = 12           # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000   # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10           # DMA channel to use for generating signal (try 10)
@@ -11,7 +11,7 @@ LED_BRIGHTNESS = 255   # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False     # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0
 LED_STRIP = ws.SK6812_STRIP_GRBW
-SLEEP = 100
+SLEEP = 1
 
 pixels = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 pixels.setGamma(common.create_gamma_table(2.2))
@@ -28,4 +28,4 @@ while True:
        for i in range(1000):
            effect.next_frame()
            pixels.show()
-           pause(10)
+           #pause(SLEEP)

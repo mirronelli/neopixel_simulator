fe15e9a089459974bbee3166a2f4e50a85279b8a
Miroslav Moravek <miroslav.moravek@2ring.com>
2022-04-19 14:47:25 +0200

    split pc and  rpi servers

---
+++ pc_server.py
@@ -0,0 +1,9 @@
+from effects import common
+from rcservice import RCService
+
+from pixels import Pixels
+pixels = Pixels(120, 2.8)
+
+if __name__ == "__main__":
+    server = RCService(pixels, "ubi", "neo", "pc", 120, 2)
+    server.run()
--- rpi_server.py
+++ rpi_server.py
@@ -1,23 +1,19 @@
 from effects import common
 from rcservice import RCService
 
-try:
-    from rpi_ws281x import PixelStrip, ws
-    LED_STRIP = ws.SK6812_STRIP_GRBW
-    LED_COUNT = 204        # Number of LED pixels.
-    LED_PIN = 12           # GPIO pin connected to the pixels (must support PWM!).
-    LED_FREQ_HZ = 800000   # LED signal frequency in hertz (usually 800khz)
-    LED_DMA = 10           # DMA channel to use for generating signal (try 10)
-    LED_BRIGHTNESS = 255   # Set to 0 for darkest and 255 for brightest
-    LED_INVERT = False     # True to invert the signal (when using NPN transistor level shift)
-    LED_CHANNEL = 0
-    pixels = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-    pixels.setGamma(common.create_gamma_table(2.8))
-    pixels.begin()
-except:
-    from pixels import Pixels
-    pixels = Pixels(120, 2.8)
+from rpi_ws281x import PixelStrip, ws
+LED_STRIP = ws.SK6812_STRIP_GRBW
+LED_COUNT = 204        # Number of LED pixels.
+LED_PIN = 12           # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ = 800000   # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10           # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255   # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False     # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0
+pixels = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+pixels.setGamma(common.create_gamma_table(2.8))
+pixels.begin()
 
 if __name__ == "__main__":
-    server = RCService(pixels, "ubi", "neo", "rpi", 120, 2)
+    server = RCService(pixels, "ubi", "neo", "pc", 120, 2)
     server.run()
